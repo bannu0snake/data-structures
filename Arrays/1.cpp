3822f5a79d43cebd2798dc18eca9492acf5b0621
bannu0snake <mahaboob.developer@gmail.com>
2022-01-03 14:10:22 +0530

    added find min max

---
+++ Arrays/1.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+struct Pair
+{
+    int min;
+    int max;
+};
+struct Pair findMinMax(int arr[], int n){
+    struct Pair minmax;    
+    int i;
+     
+    // If there is only one element
+    // then return it as min and max both
+    if (n == 1)
+    {
+        minmax.max = arr[0];
+        minmax.min = arr[0];    
+        return minmax;
+    }
+     if (arr[0] > arr[1])
+    {
+        minmax.max = arr[0];
+        minmax.min = arr[1];
+    }
+    else
+    {
+        minmax.max = arr[1];
+        minmax.min = arr[0];
+    }
+    for ( i = 2; i < n; i++)
+    {
+        if (arr[i] > minmax.max)    
+            minmax.max = arr[i];
+             
+        else if (arr[i] < minmax.min)    
+            minmax.min = arr[i];
+    }
+    return minmax;
+}
+
+int main(int argc, char const *argv[])
+{
+    int arr[] = { 1000, 11, 445,
+                  1, 330, 3000 };
+    int arr_size = 6;
+     
+    struct Pair minmax = findMinMax(arr, arr_size);
+     
+    cout << "Minimum element is "
+         << minmax.min << endl;
+    cout << "Maximum element is "
+         << minmax.max;
+          
+    return 0;
+}
